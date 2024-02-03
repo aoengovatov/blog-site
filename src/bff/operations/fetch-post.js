b8a0ee3e60b5de6e23302a5c90b78b5919d48349
AlexEngo <a.o.engovatov@yandex.ru>
2024-02-03 18:01:53 +0400

    fix bugs

--- src/bff/operations/fetch-post.js
+++ src/bff/operations/fetch-post.js
@@ -1,12 +1,22 @@
-import { getPost, getComments } from "../api";
+import { getPost, getComments, getUsers } from "../api";
 
 export const fetchPost = async (postId) => {
     const post = await getPost(postId);
 
     const comments = await getComments(postId);
 
+    const users = await getUsers();
+
+    const commentsWithAuthor = comments.map((comment) => {
+        const user = users.find(({ id }) => id === comment.authorId);
+        return {
+            ...comment,
+            author: user?.login,
+        };
+    });
+
     return {
         error: null,
-        res: { ...post, comments },
+        res: { ...post, comments: commentsWithAuthor },
     };
 };
--- src/bff/transformers/index.js
+++ src/bff/transformers/index.js
@@ -1,3 +1,4 @@
 export * from "./transform-user";
 export * from "./transform-post";
 export * from "./transrorm-session";
+export * from "./transrorm-comment";
