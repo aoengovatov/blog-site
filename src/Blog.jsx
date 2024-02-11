322b0168ad9fed12de04ef1ce694cfe3a2596c42
AlexEngo <a.o.engovatov@yandex.ru>
2024-02-11 16:40:44 +0400

    add error constants

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
-import { Header, Footer, Modal } from "./components";
+import { Error, Header, Footer, Modal } from "./components";
 import { useLayoutEffect } from "react";
+import { ERROR } from "./constants";
 import { useDispatch } from "react-redux";
 import { setUser } from "./actions";
 import { Authrization, Registration, Users, Post, Main } from "./pages";
@@ -50,7 +51,7 @@ export const Blog = () => {
                     <Route path="/post" element={<Post />} />
                     <Route path="/post/:postId" element={<Post />} />
                     <Route path="/post/:postId/edit" element={<Post />} />
-                    <Route path="*" element={<div>Ошибка</div>} />
+                    <Route path="*" element={<Error error={ERROR.PAGE_NOT_EXIST} />} />
                 </Routes>
             </Page>
             <Footer />
