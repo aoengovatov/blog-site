cbf9f1d2a2faf74b3ca8c0136c23b10b62923c22
AlexEngo <a.o.engovatov@yandex.ru>
2024-02-11 20:27:45 +0400

    add private access in page post

--- src/pages/post/post.jsx
+++ src/pages/post/post.jsx
@@ -2,19 +2,20 @@ import { useState, useEffect, useLayoutEffect } from "react";
 import { useParams, useMatch } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useServerRequest } from "../../hooks";
-import { Error } from "../../components";
+import { Error, PrivateContent } from "../../components";
 import { Comments, PostContent, PostForm } from "./components";
 import { loadPostAsync, RESET_POST_DATA } from "../../actions";
+import { ROLE } from "../../constants";
 import { selectPost } from "../../selectors";
 import styled from "styled-components";
 
 const PostContainer = ({ className }) => {
-    const [error, setError] = useState(true);
+    const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const dispatch = useDispatch();
     const params = useParams();
-    const isEditing = useMatch("/post/:postId/edit");
-    const isCreating = useMatch("/post");
+    const isEditing = !!useMatch("/post/:postId/edit");
+    const isCreating = !!useMatch("/post");
     const requestServer = useServerRequest();
     const post = useSelector(selectPost);
 
@@ -38,20 +39,21 @@ const PostContainer = ({ className }) => {
         return null;
     }
 
-    return error ? (
-        <Error error={error} />
-    ) : (
-        <div className={className}>
-            {isCreating || isEditing ? (
-                <PostForm post={post} />
-            ) : (
-                <>
-                    <PostContent post={post} />
-                    <Comments comments={post.comments} postId={post.id} />
-                </>
-            )}
-        </div>
-    );
+    const SpecificPostPage =
+        isCreating || isEditing ? (
+            <PrivateContent access={[ROLE.ADMIN]} serverError={error}>
+                <div className={className}>
+                    <PostForm post={post} />
+                </div>
+            </PrivateContent>
+        ) : (
+            <div className={className}>
+                <PostContent post={post} />
+                <Comments comments={post.comments} postId={post.id} />
+            </div>
+        );
+
+    return error ? <Error error={error} /> : SpecificPostPage;
 };
 
 export const Post = styled(PostContainer)`
