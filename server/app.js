e8dbf31c893012b2492bb47d773a135e45605ff0
AlexEngo <a.o.engovatov@yandex.ru>
2024-03-18 16:18:27 +0400

    add config

--- server/app.js
+++ server/app.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -12,14 +11,18 @@ const postForAdminRoute = require("./routes/post-for-admin.routes");
 const authentificated = require("./middlewares/authentificated");
 const hasRole = require("./middlewares/hasRole");
 
+const config = require("config");
 const ROLES = require("./constants/roles");
+
+const port = config.get("port");
+const mongoUri = config.get("mongoUri");
 
 const app = express();
 
 app.use(
     cors({
         credentials: true,
-        origin: process.env.ORIGIN,
+        origin: "http://localhost:5173",
     })
 );
 
@@ -36,8 +39,8 @@ app.use("/posts", hasRole([ROLES.ADMIN]), postForAdminRoute);
 app.use("/posts", commentRoute);
 app.use("/posts", hasRole([ROLES.ADMIN, ROLES.MODERATOR]), commentForAdminAndModerRoute);
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-    app.listen(process.env.PORT, () => {
+mongoose.connect(mongoUri).then(() => {
+    app.listen(port, () => {
         console.log(`Server start on port ${port}`);
     });
 });
